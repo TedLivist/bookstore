657cb335f9c9e9ea0d639b8b520998c91a9ba9e7
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-09-27 20:25:41 +0100

    Setup redux store and actions

---
+++ src/redux/configureStore.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import booksReducer from './books/books';
+
+const reducer = combineReducers({
+  booksReducer
+})
+
+const store = createStore(
+  reducer,
+  applyMiddleware(logger)
+)
+
+export default store;
