cdbae834d021f82e71234e42dc8f67f3576dc13c
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-09-27 17:29:08 +0100

    Setup basic Books creation and removal in React

--- src/components/books.js
+++ src/components/books.js
@@ -1,7 +1,48 @@
-import React from 'react';
+/* eslint-disable */
 
-const Books = () => (
-  <h1>Books</h1>
-);
+import React, { useState } from 'react';
+
+const Books = () => {
+  const initialBooks = [{id: 1, title: 'Book One'}, {id: 2, title: 'Book Two'}]
+
+  const [books, setBooks] = useState(initialBooks)
+  const [bookInput, setBookInput] = useState({title: ''})
+
+  const handleRemove = (id) => {
+    setBooks(books.filter((book) => book.id !== id))
+  }
+
+  const onChange = (e) => {
+    const val = e.target.value
+    setBookInput({title: val})
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    const newBook = {...bookInput}
+    setBooks(books, books.push({id: (books.length + 1), title: (newBook.title)}))
+    setBookInput({title: ''})
+  }
+
+  return (
+    <div>
+      <h1>Books</h1>
+      <ul>
+        {books.map((book) => (
+          <li key={book.id}>
+            {book.title}
+            <button type="button" onClick={() => handleRemove(book.id)}>Remove Book</button>
+          </li>
+        ))}
+      </ul>
+
+      <form onSubmit={onSubmit}>
+        <input type="text" onChange={onChange} value={bookInput.title} />
+        <button>Add Book</button>
+      </form>
+    </div>
+  );
+}
 
 export default Books;
