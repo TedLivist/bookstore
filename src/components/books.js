47376d9fb3c64fe7af7d5f09262b5068ef3881e5
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-09-27 19:52:03 +0100

    Change index of books

--- src/components/books.js
+++ src/components/books.js
@@ -1,28 +1,31 @@
 /* eslint-disable */
 
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 const Books = () => {
-  const initialBooks = [{id: 1, title: 'Book One'}, {id: 2, title: 'Book Two'}]
+  const initialBooks = [{id: 1, title: 'Book One - ', author: 'Me '}, {id: 2, title: 'Book Two - ', author: 'Em '}]
 
   const [books, setBooks] = useState(initialBooks)
-  const [bookInput, setBookInput] = useState({title: ''})
+  const [bookInput, setBookInput] = useState({title: '', author: ''})
 
   const handleRemove = (id) => {
-    setBooks(books.filter((book) => book.id !== id))
+    const filtered = books.filter((book) => book.id !== id)
+    for(let i = 0; i < filtered.length; i++) {
+      filtered[i].id = filtered.indexOf(filtered[i]) + 1
+    }
+    setBooks(filtered)
   }
 
   const onChange = (e) => {
-    const val = e.target.value
-    setBookInput({title: val})
+    setBookInput({...bookInput, [e.target.name]: e.target.value})
   }
 
   const onSubmit = (e) => {
     e.preventDefault()
 
     const newBook = {...bookInput}
-    setBooks(books, books.push({id: (books.length + 1), title: (newBook.title)}))
-    setBookInput({title: ''})
+    setBooks(books, books.push({id: (books.length + 1), title: (newBook.title), author: (newBook.author)}))
+    setBookInput({title: '', author: ''})
   }
 
   return (
@@ -31,14 +34,17 @@ const Books = () => {
       <ul>
         {books.map((book) => (
           <li key={book.id}>
+            {book.id}
             {book.title}
+            {book.author}
             <button type="button" onClick={() => handleRemove(book.id)}>Remove Book</button>
           </li>
         ))}
       </ul>
 
       <form onSubmit={onSubmit}>
-        <input type="text" onChange={onChange} value={bookInput.title} />
+        <input type="text" name='title' onChange={onChange} value={bookInput.title} placeholder="Title" />
+        <input type="text" name='author' onChange={onChange} value={bookInput.author} placeholder="Author" />
         <button>Add Book</button>
       </form>
     </div>
